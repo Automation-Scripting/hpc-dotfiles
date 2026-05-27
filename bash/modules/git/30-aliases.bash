c4302dc22029d267fefbb754eb63f72aa1d546c2
Nara Avila <nara.moraes@usp.br>
2026-05-27 09:44:51 -0300

    Update files

--- bash/modules/git/30-aliases.bash
+++ bash/modules/git/30-aliases.bash
@@ -3,3 +3,4 @@
 # Compatibilidade com aliases antigos do fluxo diario
 alias u="git pull && git add -A && git commit -m 'Update files' && git push; clear"
 alias n="git pull; clear"
+alias dotpull='git -C "$HOME/.dotfiles" pull --rebase'
